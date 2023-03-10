254d1355c5afbfc61829f1263a1f9dc17a60774f
Eric-Max-Chrisman <122303933+Eric-Max-Chrisman@users.noreply.github.com>
2023-03-10 08:56:24 -0600

    Update Author.ts

--- src/entities/Author.ts
+++ src/entities/Author.ts
@@ -1,1 +1,17 @@
-tem
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, Relation } from 'typeorm';
+import { Review };
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn('uuid')
+  authorId: string;
+
+  @Column({ unique: false })
+  authorName: string;
+  
+  @Column({ unique: false, default: 'unknown' })
+  countryOrgin: string;
+
+  @ManyToMany(() => Book, (book) => book.bookId, { cascade: true })
+  books: Relation<Book>[];
+}
